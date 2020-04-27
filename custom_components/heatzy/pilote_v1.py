542f84e4a3467f80f0aae4e41c155eb77917f205
cyr-ius <cyr-ius@ipocus.net>
2020-04-27 15:19:44 +0200

    Fix lint syntax

--- custom_components/heatzy/pilote_v1.py
+++ custom_components/heatzy/pilote_v1.py
@@ -72,7 +72,6 @@ class HeatzyPiloteV1Thermostat(ClimateEntity):
     @property
     def device_info(self):
         """Return the device info."""
-
         return {
             "name": self.name,
             "identifiers": {(DOMAIN, self.unique_id)},
@@ -119,7 +118,7 @@ class HeatzyPiloteV1Thermostat(ClimateEntity):
         """Set new preset mode."""
         await self.hass.async_add_executor_job(
             self._api.control_device,
-            self.unique_id, 
+            self.unique_id,
             {"raw": HA_TO_HEATZY_STATE.get(preset_mode)}
         )       
         await self.async_update_heater(True)
