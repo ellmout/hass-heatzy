b40e328ce072f1e3ffb71d9fcc147c141b408991
cob94440 <62489067+cob94440@users.noreply.github.com>
2021-03-14 13:59:30 +0100

    Update climate.py

--- custom_components/heatzy/climate.py
+++ custom_components/heatzy/climate.py
@@ -35,6 +35,8 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             devices.append(HeatzyPiloteV1Thermostat(coordinator, device["did"]))
         elif product_key in PILOTEV2 or product_key in ELEC_PRO_SOC:
             devices.append(HeatzyPiloteV2Thermostat(coordinator, device["did"]))
+        if product_key in GLOW:
+            devices.append(HeatzyGlow(coordinator, device["did"]))
     async_add_entities(devices)
 
 
@@ -162,3 +164,44 @@ class HeatzyPiloteV2Thermostat(HeatzyThermostat):
             await self.coordinator.async_request_refresh()
         except HeatzyException as error:
             _LOGGER.error("Error to set preset mode : %s", error)
+        await self.async_update_heater(True)
+
+
+class HeatzyGlow(HeatzyThermostat):
+    """Heaty Glow."""
+
+    HEATZY_TO_HA_STATE = {
+        "cft": PRESET_COMFORT,
+        "eco": PRESET_ECO,
+        "fro": PRESET_AWAY,
+        "stop": PRESET_NONE,
+    }
+
+    HA_TO_HEATZY_STATE = {
+        PRESET_COMFORT: "cft",
+        PRESET_ECO: "eco",
+        PRESET_AWAY: "fro",
+        PRESET_NONE: "stop",
+    }
+
+    @property
+    def preset_mode(self):
+        """Return the current preset mode, e.g., home, away, temp.
+
+        Requires SUPPORT_PRESET_MODE.
+        """
+        return self.HEATZY_TO_HA_STATE.get(
+            self._heater_data.get("attr", {}).get("mode")
+        )
+
+    async def async_set_preset_mode(self, preset_mode):
+        """Set new preset mode."""
+        try:
+            await self.hass.async_add_executor_job(
+                self._api.control_device,
+                self.unique_id,
+                {"attrs": {"mode": self.HA_TO_HEATZY_STATE.get(preset_mode)}},
+            )
+        except HeatzyException as error:
+            _LOGGER.error("Error to set preset mode : %s", error)
+        await self.async_update_heater(True)
