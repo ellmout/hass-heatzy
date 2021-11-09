f360d32a8a3ca1bc9167bac6b9d7d41a41acd417
cyr-ius <cyr-ius@ipocus.net>
2021-11-09 10:13:14 +0100

    Add lock mode

--- custom_components/heatzy/__init__.py
+++ custom_components/heatzy/__init__.py
@@ -1,18 +1,18 @@
 """Heatzy platform configuration."""
-from datetime import timedelta
+import asyncio
 import logging
+from datetime import timedelta
 
 import async_timeout
 from heatzypy import HeatzyClient
 from heatzypy.exception import HeatzyException, HttpRequestFailed
-
-from homeassistant.components.climate.const import DOMAIN as CLIM_DOMAIN
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.debounce import Debouncer
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.update_coordinator import (DataUpdateCoordinator,
+                                                      UpdateFailed)
 
-from .const import DEBOUNCE_COOLDOWN, DOMAIN
+from .const import COMPONENTS, DEBOUNCE_COOLDOWN, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 UPDATE_INTERVAL = timedelta(minutes=1)
@@ -25,20 +25,33 @@ async def async_setup_entry(hass, config_entry):
     coordinator = HeatzyDataUpdateCoordinator(hass, config_entry)
     await coordinator.async_config_entry_first_refresh()
 
+    if coordinator.data is None:
+        return False
+
     hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, CLIM_DOMAIN)
-    )
+    for component in COMPONENTS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(config_entry, component)
+        )
+
     return True
 
 
 async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(
-        config_entry, [CLIM_DOMAIN]
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(config_entry, component)
+                for component in COMPONENTS
+            ]
+        )
     )
-    return unload_ok
+    if unload_ok:
+        hass.data[DOMAIN].pop(config_entry.entry_id)
+
+    return True
 
 
 class HeatzyDataUpdateCoordinator(DataUpdateCoordinator):
@@ -64,7 +77,7 @@ class HeatzyDataUpdateCoordinator(DataUpdateCoordinator):
         )
 
     async def _async_update_data(self) -> dict:
-        with async_timeout.timeout(10):
+        with async_timeout.timeout(30):
             try:
                 devices = await self.hass.async_add_executor_job(
                     self.heatzy_client.get_devices
